6fc60981362182d4b77d55b0009fa3284c2f21ce
Leonardo Paschoa <78983450+LeonardoPaschoaBraga16@users.noreply.github.com>
2025-05-14 15:48:30 -0300

    Projeto Estilizado
    Estilo Finalizado

--- index.php
+++ index.php
@@ -3,15 +3,21 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap" rel="stylesheet">
     <title>Teste</title>
 </head>
 <body>
     <h1>Consultar Cep</h1>
 
-    <input type="text" id="cep" placeholder="Digite seu CEP (ex: 12345678)">
-    <button onclick="consultarCEP()">Buscar informações</button>
+    <div class="buttons">
+        <input type="text" id="cep" placeholder="Digite seu CEP (ex: 12345678)">
+        <button onclick="consultarCEP()">Buscar informações</button>
+    </div>
 
-    <div class="resultado" id="resultado">
+    <div class="resultado flex" id="resultado">
 
     </div>
 
